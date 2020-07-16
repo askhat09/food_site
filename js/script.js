a49b226bd6084d9c0fdb354332286db47cb96d24
Askhat <s.askhat.b@gmail.com>
2020-07-16 17:41:32 +0600

    tabs with class

--- js/script.js
+++ js/script.js
@@ -136,4 +136,68 @@ window.addEventListener('DOMContentLoaded', () => {
 
         window.addEventListener('scroll', showModalByScroll);
 
+        // Use class for card
+
+
+        class MenuCard {
+            constructor(src, alt, subtitle, descr, price, parentSelector) {
+                this.src = src;
+                this.alt = alt;
+                this.subtitle = subtitle;
+                this.descr = descr;
+                this.price = price;
+                this.transfer = 27;
+                this.parentSelector = document.querySelector(parentSelector);
+                this.changeMoney();
+            }
+
+            changeMoney() {
+                this.price = this.price * this.transfer;
+            }
+
+            render() {
+                const div = document.createElement('div');
+                div.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+                `;
+                this.parentSelector.append(div);
+            }          
+        }
+
+        new MenuCard(
+            "img/tabs/vegy.jpg",
+            "vegy",
+            'Меню "Фитнес"',
+            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            15,
+            '.menu .container'
+        ).render();
+
+        new MenuCard(
+            "img/tabs/elite.jpg",
+            "elite",
+            'Меню “Премиум”',
+            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            12,
+            '.menu .container'
+        ).render();
+
+        new MenuCard(
+            "img/tabs/post.jpg",
+            "post",
+            'Меню "Постное"',
+            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            13,
+            '.menu .container'
+        ).render();
+
 });
