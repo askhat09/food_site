68b032e7026689e3789ee7b672d2ee460c98f126
Askhat <s.askhat.b@gmail.com>
2020-07-21 22:32:52 +0600

    Установка json сервера, работа json файлом

--- js/script.js
+++ js/script.js
@@ -273,4 +273,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000)
     }
 
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res))
+
 });
